33d93430f90439bd6013852671ac162eeca90029
Patrick Hulce <patrick.hulce@gmail.com>
2023-12-26 13:25:37 -0600

    refactor: extract N seconds to var

--- scripts/extract-frames.sh
+++ scripts/extract-frames.sh
@@ -6,6 +6,7 @@ INPUT_FILE=${1:-data/video/sample.mp4}
 OUTPUT_DIR=${2:-output}
 FORCE="no"
 OFFSET_SECONDS=0
+EVERY_N_SECONDS=5
 
 # Get duration in seconds
 duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$INPUT_FILE")
@@ -13,7 +14,7 @@ duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wr
 # Convert to integer
 duration=${duration%.*}
 
-for ((i=OFFSET_SECONDS; i<=duration; i+=5)); do
+for ((i=OFFSET_SECONDS; i<=duration; i+=EVERY_N_SECONDS)); do
     time=$(printf "%02d:%02d:%02d" $((i/3600)) $(( (i/60)%60)) $((i%60)) )
     # Replace the colons with hyphens
     time_safe=${time//:/-}
